dbf983d9858ebee4d1f23d339af9a237ad4476d6
thedesignerjane <122414213+thedesignerjane@users.noreply.github.com>
2023-04-18 18:03:50 -0400

    Update map.js
    updates

--- Clean Final/script/map.js
+++ Clean Final/script/map.js
@@ -138,8 +138,11 @@ var newData = ridershipData.map((d, index) => ({
   console.log(newData);
 
   return newData
-};
 
+
+
+
+};
 
 let geoJSONFile = "https://gist.githubusercontent.com/jdev42092/5c285c4a3608eb9f9864f5da27db4e49/raw/a1c33b1432ca2948f14f656cc14c7c7335f78d95/boston_neighborhoods.json";
 
@@ -172,9 +175,9 @@ const drawMap = async () => {
       .enter()
           .append("path")
           .attr("d", path)
-          .attr("fill", "#F4EADC")
+          .attr("fill", "#D3DEF2")
           .attr("vector-effect", "non-scaling-stroke")
-          .attr("stroke", "#ED8B00")
+          .attr("stroke", "#A0BBEC")
           .attr("stroke-width", "1.0");
 
   const circles = map.selectAll("circle")
@@ -281,3 +284,108 @@ const drawMap = async () => {
 }
 
 drawMap();
+
+//Choroplethmap
+
+const cmapcontainer = document.getElementById("cmap-container");
+    const cmapwidth = container.clientWidth, cmapheight = container.clientHeight;
+
+    const cmapsvg = d3.select("#cviz")
+                .attr("width", cmapwidth)
+                .attr("height", cmapheight);
+
+    const cmap = cmapsvg.select("#cmap");
+
+    d3.select("#cocean")
+    .attr("width", cmapwidth)
+    .attr("height", cmapheight);
+
+    let geoJSONFile2 = "https://gist.githubusercontent.com/jdev42092/5c285c4a3608eb9f9864f5da27db4e49/raw/a1c33b1432ca2948f14f656cc14c7c7335f78d95/boston_neighborhoods.json";
+
+    d3.json(geoJSONFile2).then(function(ditu) {
+
+        // var proj = d3.geoMercator().fitSize([width, height], ditu);
+
+        /**
+         * Optionally, use this projection instead of the one above. 
+         * Its not much different in terms of the resulting map, but it just adds
+         * some realism in terms of Boston's actual longitude and latitude
+         */
+
+        var proj = d3.geoMercator()
+            .fitSize([cmapwidth, cmapheight], ditu)
+            // Optionally, add these
+            .rotate( [71.057,0] ) // Boston's longitude
+            .center( [0, 42.313] ) // Boston's latitude
+            // Translate the map to the center of the screen
+            .translate( [cmapwidth/2,cmapheight/2] );
+
+        var path = d3.geoPath().projection(proj);
+
+        cmap.selectAll("path")
+            .data(ditu.features)
+            .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("fill", "#D3DEF2")
+                .attr("vector-effect", "non-scaling-stroke")
+                .attr("stroke", "#A0BBEC")
+                .attr("stroke-width", "1px");
+        
+        // var points = [
+        //     {"name": "Boston", "coords": [-71.057, 42.313]}
+        // ];
+
+        // var circleRadius = 10;
+
+        // map.selectAll("circle")
+        //     .data(points)
+        //     .enter()
+        //         .append("circle")
+        //         .attr("r", circleRadius)
+        //         .attr("fill", "#201E20")
+        //         .attr("transform", function(d) {
+        //             return "translate(" + proj(d.coords) + ")";
+        //         });
+
+        function zoomed(e) {
+            cmap.attr("transform", e.transform);
+        };
+
+        let zoom = d3.zoom()
+        
+            .translateExtent([[0, 0], [cmapwidth, cmapheight]])
+
+            .scaleExtent([1, 30])
+            .on("zoom", zoomed);
+
+        cmapsvg.call(zoom);
+
+  // An object that maps each station to its corresponding neighborhood
+  const stationNeighborhoods = {};
+
+  // Loop through newData to create the stationNeighborhoods mapping
+  for (const station of newData) {
+    stationNeighborhoods[station.Stationname] = station.neighborhood;
+  }
+
+  // An object to store the total ridership for each neighborhood
+  const neighborhoodRidership = {};
+
+  // Loop through newData to compute the total ridership for each neighborhood
+  for (const station of newData) {
+    const neighborhood = station.neighborhood;
+    const ridershipByYear = station.ridership_by_year;
+
+    if (neighborhood in neighborhoodRidership) {
+      // Add the ridership for this station to the total for the neighborhood
+      for (let i = 0; i < ridershipByYear.length; i++) {
+        neighborhoodRidership[neighborhood][i] += ridershipByYear[i];
+      }
+    } else {
+      // Initialize the total ridership for this neighborhood
+      neighborhoodRidership[neighborhood] = [...ridershipByYear];
+    }
+  }
+  console.log(neighborhoodRidership);
+});
